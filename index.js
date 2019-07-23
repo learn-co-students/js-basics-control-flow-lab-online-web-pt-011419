d1d0cb11fbc2b8c9b1fd39d5ec7181e89f2153f2
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-07-23 18:04:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message = '';
+
+  if(distance > 1 && distance <= 400) {
+    message = 'This one is on me!';
+  }
+  else if(distance > 2500) {
+    message = 'No can do.';
+  }
+  else if(distance > 2000 && distance < 2500 ) {
+    message = 'I will gladly take your thirty bucks.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message = '';
+
+  city === "NYC" ?  message = "Ok, sounds good." : message = "No go.";
+
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message = '';
+
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+
+  }
+
+  return message 
+
 }

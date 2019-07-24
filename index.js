8e979789e7ace4dc6418e6271863c1c2769144ce
Jenniekerf <jenniekerf@gmail.com>
2019-07-24 04:52:58 +0200

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,27 @@
 function scuberGreetingForFeet(feet){
-  // Write your code here!
-  let greeting;
   if (feet <= 400) {
-    greeting = 'This one is on me!'
+    return 'This one is on me!'
   }
   else if (feet < 2500 && feet > 1999) {
-  greeting = 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.'
 }
   else {
-    greeting = 'No can do.'
+    return 'No can do.'
   }
-  return greeting
 }
 
-function ternaryCheckCity(city){
-  // Write your code here!
-if (city === 'NYC') {
-  return 'Ok, sounds good.'
-}
-else {
-return 'No go.'
-}
+function ternaryCheckCity (city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip){
-  // Write your code here!
-
   switch (tip) {
     case 'generous' :
       return 'Thank you so much.';
-      break;
     case 'not as generous' :
       return 'Thank you.';
-      break;
-    case 'thanks for everything' :
+    default:
       return 'Bye.';
-      break;
 
 }
 }

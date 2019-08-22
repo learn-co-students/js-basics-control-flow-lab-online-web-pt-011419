fe123dd06024514a7eeac9221d5a3fb271a93719
LaRaye Johnson <lnj2105@gmail.com>
2019-08-22 04:22:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+  let riderMessage
+  
+  if (someFeet <= 400) {
+    riderMessage = 'This one is on me!'
+  } else if (someFeet >= 2500) {
+    riderMessage = 'No can do.'
+  } else if (someFeet >= 2000) {
+    riderMessage = 'I will gladly take your thirty bucks.'
+  }
+  
+  return riderMessage
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  let cityMessage
+  someCity === 'NYC' ? cityMessage = 'Ok, sounds good.' :  cityMessage = 'No go.'
+  return cityMessage
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipMessage
+  
+  switch (tip) {
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      tipMessage = 'Bye.';
+      break;
+  }
+  return tipMessage
 }
